fc3121bdf031c67bae9ec6d940ab3d3f3d9497eb
DEV270201 <devansh.js@somaiya.edu>
2021-04-09 16:56:48 +0530

    modified themecontext file

--- src/Context/ThemeContextProvider.js
+++ src/Context/ThemeContextProvider.js
@@ -3,9 +3,25 @@ import React,{createContext,useState} from "react";
 export const ThemeContext = createContext();
 
 export const ThemeContextProvider = (props)=>{
+    const[theme,setTheme] = useState("");
+    const[theme_status,setTheme_status] = useState("light");
+
+    const changeTheme = (passedTheme)=>{
+       if(theme === "light"){
+        //    setTheme(passedTheme);
+           setTheme_status(passedTheme);
+       }else{
+        setTheme_status(passedTheme);
+       }
+    }
+
     return(
         <>
-        <h1>Context</h1>
+        <ThemeContext.Provider value={{theme_status , changeTheme}}>
+             {
+                 props.children
+             }
+        </ThemeContext.Provider>
         </>
     );
 }

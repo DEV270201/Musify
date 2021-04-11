c19ff8988b8a7558c60aa8ba567bc3e11fcc0002
DEV270201 <devansh.js@somaiya.edu>
2021-04-11 13:00:53 +0530

    added new songs

--- src/musicData/Data.js
+++ src/musicData/Data.js
@@ -1,6 +1,11 @@
 import Music1 from "../Music/Memories.mp3";
 import Music3 from "../Music/Junoon.mp3";
 import Music2 from "../Music/IllahiInstrument.mp3";
+import Music4 from "../Music/Sapna_Jahaan.mpeg";
+import Music5 from "../Music/Gulabi Aankhen.mpeg";
+import Music6 from "../Music/Pal Pal Dil.mpeg";
+
+
 
 const Data = [
     {
@@ -29,7 +34,35 @@ const Data = [
         end : 331,
         isLiked : false,
         isRepeat : false,
+    },
+    {
+        src : "https://a10.gaanacdn.com/images/albums/14/1410914/crop_480x480_1410914.jpg",
+        name : "Sapna Jahaan",
+        artist : "Ajay-Atul",
+        audio : Music4,
+        end : 341,
+        isLiked : false,
+        isRepeat : false,
+    },
+    {
+        src : "https://a10.gaanacdn.com/gn_img/albums/21GWwR3pkg/1GWwwaxpWp/size_xxl.jpg",
+        name : "Gulabi Aankhen",
+        artist : "Sanam",
+        audio : Music5,
+        end : 224,
+        isLiked : false,
+        isRepeat : false,
+    },
+    {
+        src : "https://1.bp.blogspot.com/-6lfvXM4-7Hs/XqguhpM23QI/AAAAAAAAAKk/KMas7NQGf8wYybrt078WmFTFXOXf0rZAwCEwYBhgL/s1600/pal-pal-dil-ke-paas-lyrics.jpg",
+        name : "Pal Pal Dil ",
+        artist : "Kishore Kumar",
+        audio : Music6,
+        end : 329,
+        isLiked : false,
+        isRepeat : false,
     }
+    
 ];
 
 export default Data;

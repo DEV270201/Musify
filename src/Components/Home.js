d0017f286156b415d3135d446717b966f098eb65
DEV270201 <devansh.js@somaiya.edu>
2021-04-18 20:36:43 +0530

    modified files

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,8 @@ import Background from "./Background";
 import AboutUs from "./AboutUs";
 import MusicCompo from "./MusicCompo";
 import MyMap from "./Map";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons';
 
 const Home = ()=>{
   return(
@@ -13,7 +15,7 @@ const Home = ()=>{
       <AboutUs />
       <MusicCompo />
       <div id="mymap">
-      <h1>Map</h1>
+      <h2 className="map_head"><FontAwesomeIcon  icon={faMapMarkerAlt}/>       Our Headquarters</h2>
       <div style={{border : "1px solid white",padding : "10px", borderRadius : "10px"}}>
       <MyMap />
       </div>
--- src/Components/MusicCompo.js
+++ src/Components/MusicCompo.js
@@ -7,7 +7,7 @@ import "../Css/MusicCompo.css";
 const MusicCompo = ()=>{
    return(
        <>
-         <h2 class="music_compo_head">Music Player Design</h2>
+         <h2 className="music_compo_head">Music Player Design</h2>
        <div className="music_compo_outer" style={{backgroundImage: `url("https://images.unsplash.com/photo-1489797715492-dbd3bd61c6b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80")`}}>
         <div className="music_compo_inner">
          <div className="steps1">

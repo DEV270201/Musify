b967d7f513a5f7ab3abf1c6d1fb3c1cff5e8b020
DEV270201 <devansh.js@somaiya.edu>
2021-04-27 10:07:10 +0530

    modified

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -4,13 +4,13 @@ import "../Css/PopUp.css";
 const PopUp = ({office}) => {
     return (
         <>
-            <div class="info">
-                <div class="info_head">
-                    <h4 class="info_head1">{office.properties.Name}</h4>
+            <div className="info">
+                <div className="info_head">
+                    <h4 className="info_head1">{office.properties.Name}</h4>
                 </div>
-                <div class="add_info">
-                    <p class="add_info1">{office.properties.Address}</p>
-                    <a class="add_info2" href={`tel:${office.properties.TelNo}`}>{office.properties.TelNo}</a>
+                <div className="add_info">
+                    <p className="add_info1">{office.properties.Address}</p>
+                    <a className="add_info2" href={`tel:${office.properties.TelNo}`}>{office.properties.TelNo}</a>
                 </div>
             </div>
         </>

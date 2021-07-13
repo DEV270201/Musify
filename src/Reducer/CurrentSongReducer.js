279f39534a0d72ac03131b81a0f4f8d1359194af
DEV270201 <devansh.js@somaiya.edu>
2021-07-13 19:59:12 +0530

    added condition

--- src/Reducer/CurrentSongReducer.js
+++ src/Reducer/CurrentSongReducer.js
@@ -5,7 +5,14 @@ const CurrentSongReducer = (state,action)=>{
   switch(action.type){
       case "CHANGE_MUSIC":
         console.log("event fired");
-        return Data[action.payload.id];
+        console.log(Data[action.payload.id]);
+        //if at all during the shuffle function if the random gives us the same song which was played currently then we will return the next song
+        if(state.id === action.payload.id){
+          console.log('same song came again');
+          return Data[(action.payload.id + 1) % Data.length];
+        }else{
+          return Data[action.payload.id];
+        }
       default:
         return state;
   }

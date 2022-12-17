616cc232dd3fb7ee9d78186a81be28258fff8f62
DEV270201 <devansh.js@somaiya.edu>
2022-12-17 16:28:31 +0530

    changes

--- client/src/Reducer/CurrentSongReducer.js
+++ client/src/Reducer/CurrentSongReducer.js
@@ -1,21 +1,23 @@
-//CURRENTSONG REDUCER
-import Data from "../MusicData/Data";
+// //CURRENTSONG REDUCER
+// import Data from "../MusicData/Data";
 
-const CurrentSongReducer = (state,action)=>{
-  switch(action.type){
-      case "CHANGE_MUSIC":
-        // console.log("event fired");
-        // console.log(Data[action.payload.id]);
-        //if at all during the shuffle function if the random gives us the same song which was played currently then we will return the next song
-        if(state.id === action.payload.id){
-          // console.log('same song came again');
-          return Data[(action.payload.id + 1) % Data.length];
-        }else{
-          return Data[action.payload.id];
-        }
-      default:
-        return state;
-  }
-}
+// const CurrentSongReducer = (state,action)=>{
+//   switch(action.type){
+//       case "CHANGE_MUSIC":
+//         // console.log("event fired");
+//         // console.log(Data[action.payload.id]);
+//         //if at all during the shuffle function if the random gives us the same song which was played currently then we will return the next song
+//         if(state.id === action.payload.id){
+//           // console.log('same song came again');
+//           return Data[(action.payload.id + 1) % Data.length];
+//         }else{
+//           return Data[action.payload.id];
+//         }
+//       default:
+//         return state;
+//   }
+// }
 
-export default CurrentSongReducer;
+// export default CurrentSongReducer;
+
+//no longer using reducer hook
